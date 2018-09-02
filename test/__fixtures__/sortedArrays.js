71a3854d8e39cb92f49867f00c461384b45c0169
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-09-02 14:51:53 +0100

    test: add input and tree log fixtures

---
+++ test/__fixtures__/sortedArrays.js
@@ -0,0 +1,43 @@
+const sortedArray2 = [
+  { id: "001", name: "Alice"},
+  { id: "002", name: "Bob"},
+]
+
+const sortedArray6 = [
+  { id: "001", name: "Alice"},
+  { id: "002", name: "Bob"},
+  { id: "003", name: "Carl"},
+  { id: "004", name: "Dennis"},
+  { id: "005", name: "Eric"},
+  { id: "006", name: "Frank"},
+]
+
+const sortedArray7 = [
+  { id: "001", name: "Alice"},
+  { id: "002", name: "Bob"},
+  { id: "003", name: "Carl"},
+  { id: "004", name: "Dennis"},
+  { id: "005", name: "Eric"},
+  { id: "006", name: "Frank"},
+  { id: "007", name: "Gary"},
+]
+
+const sortedArray10 = [
+  { id: "001", name: "Alice"},
+  { id: "002", name: "Bob"},
+  { id: "003", name: "Carl"},
+  { id: "004", name: "Dennis"},
+  { id: "005", name: "Eric"},
+  { id: "006", name: "Frank"},
+  { id: "007", name: "Gary"},
+  { id: "008", name: "Harriet"},
+  { id: "009", name: "Iris"},
+  { id: "0010", name: "James"},
+]
+
+module.exports = {
+  sortedArray2,
+  sortedArray6,
+  sortedArray7,
+  sortedArray10
+}
---
+++ test/__fixtures__/treeLogs.js
@@ -0,0 +1,26 @@
+const tree2 = `
+ 1
+0`
+
+const tree6 = `
+   3
+ 1   5
+0 2 4`
+
+const tree7 = `
+   3
+ 1   5
+0 2 4 6`
+
+const tree10 = `
+       7
+   3
+ 1   5   9
+0 2 4 6 8`
+
+module.exports = {
+  tree2,
+  tree6,
+  tree7,
+  tree10
+}
